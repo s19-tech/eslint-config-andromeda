a43615985fe6ce72b87c8a0864c442a6d9d0c811
Ivysaur <contact@pchennz.com>
2017-04-24 13:37:11 +0100

    alias config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,46 @@
+const path = require('path');
+
 module.exports = {
-  "extends": [
-    "eslint-config-airbnb",
+  extends: [
+    'eslint-config-airbnb',
   ].map(require.resolve),
 
-  "plugins": [
-    "react",
+  plugins: [
+    'react',
   ],
 
-  "parser": "babel-eslint",
+  parser: 'babel-eslint',
 
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true,
-      "modules": true,
-      "experimentalObjectRestSpread": true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
     },
   },
 
-  "rules": {
-    "jsx-a11y/no-static-element-interactions": "off",
-    "linebreak-style": "off",
-    "no-use-before-define": ["error", { "functions": false }],
-    "react/jsx-filename-extension": "off",
-    "react/no-unused-prop-types": "off",
+  rules: {
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'linebreak-style': 'off',
+    'no-use-before-define': ['error', { functions: false }],
+    'react/jsx-filename-extension': 'off',
+    'react/no-unused-prop-types': 'off',
+  },
+
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            extensions: ['.js', '.json'],
+            alias: {
+              Assets: path.resolve('.', 'static', 'assets'),
+              Components: path.resolve('.', 'app', 'components'),
+              Containers: path.resolve('.', 'app', 'containers'),
+            },
+          },
+        },
+      },
+    },
   },
 };
