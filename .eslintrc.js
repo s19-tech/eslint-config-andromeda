d6e3bade545d197bef20f7f2afd5031121c94279
Elliot Theis <elliottheis@networkguru.org.uk>
2017-09-04 16:34:27 +0100

    tidying things up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,21 @@
 module.exports = {
-  extends: ['eslint-config-airbnb'].map(require.resolve),
+  extends: ['airbnb'],
 
-  plugins: ['react', 'babel'],
+  plugins: ['babel'],
 
   parser: 'babel-eslint',
 
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-      experimentalObjectRestSpread: true,
-    },
-  },
-
   rules: {
     'jsx-a11y/no-static-element-interactions': 'off',
-    'linebreak-style': 'off',
     'no-use-before-define': ['error', { functions: false }],
     'react/jsx-filename-extension': 'off',
     'react/no-unused-prop-types': 'off',
     'max-len': ['error', 80],
-    'arrow-parens': ['error', 'as-needed'],
+    'arrow-parens': ['error', 'as-needed']
   },
   settings: {
     'import/resolver': {
-      'babel-module': {},
+      'babel-module': {}
     },
   },
 };
